071931259759051eb4a58ef29c5b6b8b410f53b4
chocksaway <milesd@chocksaway.com>
2015-12-08 23:14:02 +0000

    Initial Import

---
+++ src/main/java/com/chocksaway/CalendarController.java
@@ -0,0 +1,14 @@
+package com.chocksaway;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalendarController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
